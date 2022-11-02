198481853e5ef382be34b23937a492444a40d64b
Felix D.H Kang(Air) <cozyfex@gmail.com>
2022-11-02 15:50:16 +0900

    Initial Commit

---
+++ WebView/WebViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewApp.swift
+//  WebView
+//
+//  Created by CozyFex on 2022/11/02.
+//
+
+import SwiftUI
+
+@main
+struct WebViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
